2d05d21e3f91b1fc256799cfcd19a2acd1a50a72
TomCastagnino <tcastagnino@gmail.com>
2018-02-14 15:00:38 +0000

    typo

--- public/js/trabajos.js
+++ public/js/trabajos.js
@@ -4,7 +4,7 @@ $('.carousel').on('slide.bs.carousel', function () {
     $('#texto').html("Animaciones y sonidos activados por el teclado. Una recreación de patatap.com.  <br>{PaperJS y Howler.JS}");
   }
   if ($('#segundo').hasClass("active")) {
-    $('#texto').html("Un juego para aprender a identificar RGBs. <br>{HTML, CSS, JS y DOM manipulation}");
+    $('#texto').html("Un juego para aprender a identificar RGBs. <br>{HTML, CSS, Vainilla JS}");
   }
   if ($('#tercero').hasClass("active")) {
     $('#texto').html("Un sitio para compartir cervecerías.  <br>{RESTful routes, base de datos, autenticación, Google APIs y Bootstrap}");
