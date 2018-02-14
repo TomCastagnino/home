a77cafe752c631d2b387fb81fa0d257159dcdf89
TomCastagnino <tcastagnino@gmail.com>
2018-02-14 02:58:09 +0000

    modifico el carousel usando jquerry, agrego alguno retoques de estilo

---
+++ public/js/trabajos.js
@@ -0,0 +1,12 @@
+
+$('.carousel').on('slide.bs.carousel', function () {
+  if ($('#primero').hasClass("active")) {
+    $('#texto').html("Animaciones y sonidos activados por el teclado. Una recreación de patatap.com.  <br>{PaperJS y Howler.JS}");
+  }
+  if ($('#segundo').hasClass("active")) {
+    $('#texto').html("Un juego para aprender a identificar RGBs. <br>{HTML, CSS, JS y DOM manipulation}");
+  }
+  if ($('#tercero').hasClass("active")) {
+    $('#texto').html("Un sitio para compartir cervecerías.  <br>{RESTful routes, base de datos, autenticación, Google APIs y Bootstrap}");
+    }
+});
