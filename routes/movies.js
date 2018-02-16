d95d98da76265d5822af2ce1d334c185750338f6
TomCastagnino <tcastagnino@gmail.com>
2018-02-16 14:46:21 +0000

    movie page style

--- routes/movies.js
+++ routes/movies.js
@@ -19,11 +19,23 @@ router.get("/trabajos/d/results", function (req, res) {
     request(url+query+process.env.MOVIE_KEY , function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var data = JSON.parse(body);
-            console.log(data);
-            res.render("movies/movieResults", {data: data});
+            res.render("movies/movieResults", {data: data, search: query});
         }
     });
 });
+
+router.get("/trabajos/d/info/:id", function (req, res) {
+    var url = "http://www.omdbapi.com/?i=";
+    var imdb = req.params.id;
+    var fullPlot = "&plot=full";
+    request(url+imdb+fullPlot+process.env.MOVIE_KEY , function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            res.render("movies/movieInfo", {data: data});
+        }
+    });
+});
+
 
 
 module.exports = router;
