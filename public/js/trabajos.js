83e71574a26b9266f0890e033c9b6a2c0611f8e3
TomCastagnino <tcastagnino@gmail.com>
2018-02-18 16:30:26 +0000

    fix a carousel bug

--- public/js/trabajos.js
+++ public/js/trabajos.js
@@ -1,22 +1,22 @@
 
-$('.carousel').on('slide.bs.carousel', function () {
+$('.carousel').on('slid.bs.carousel', function () {
   if ($('#primero').hasClass("active")) {
-    $('#texto').html("Animaciones y sonidos activados por el teclado. Una recreación de patatap.com.  <br>{PaperJS y Howler.JS}");
-  }
-  if ($('#segundo').hasClass("active")) {
-    $('#texto').html("Un juego para aprender a identificar RGBs. <br>{HTML, CSS, Vainilla JS}");
-  }
-  if ($('#tercero').hasClass("active")) {
-    $('#texto').html("Base de datos de películas. <br>{Semantic UI, APIs}");
-    }   
-  if ($('#cuarto').hasClass("active")) {
-    $('#texto').html("Un juego para saltar nubes. <br>{Unity, C#}");
-    }   
-  if ($('#quinto').hasClass("active")) {
-    $('#texto').html("Un clon del Flappy Bird.  <br>{Unity, C#}");
-    } 
-  if ($('#sexto').hasClass("active")) {
     $('#texto').html("Un sitio para compartir cervecerías.  <br>{RESTful routes, base de datos, autenticación, Google APIs y Bootstrap}");
     }
+  if ($('#segundo').hasClass("active")) {
+    $('#texto').html("Animaciones y sonidos activados por el teclado. Una recreación de patatap.com.  <br>{PaperJS y Howler.JS}");
+  }
+  if ($('#tercero').hasClass("active")) {
+    $('#texto').html("Un juego para aprender a identificar RGBs. <br>{HTML, CSS, Vainilla JS}");
+  }
+  if ($('#cuarto').hasClass("active")) {
+    $('#texto').html("Base de datos de películas. <br>{Semantic UI, APIs}");
+    }   
+  if ($('#quinto').hasClass("active")) {
+    $('#texto').html("Un juego para saltar nubes. <br>{Unity, C#}");
+    }   
+  if ($('#sexto').hasClass("active")) {
+    $('#texto').html("Un clon del Flappy Bird.  <br>{Unity, C#}");
+    } 
 });
 
