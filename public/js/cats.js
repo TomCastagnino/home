7688a025906f04331f82bb11f742fa30e967f38a
TomCastagnino <tcastagnino@gmail.com>
2018-02-21 18:57:55 +0000

    cats javascript

---
+++ public/js/cats.js
@@ -0,0 +1,17 @@
+var button = document.querySelector("#btn");
+var img = document.querySelector("#photo");
+
+button.addEventListener("click", function() {
+  var XHR = new XMLHttpRequest();
+  XHR.onreadystatechange = function() {
+    if(XHR.readyState == 4 && XHR.status == 200) {
+      var xml = XHR.responseXML;
+      var source = xml.getElementsByTagName("source_url");
+      var  innerSource = source[0]["innerHTML"];
+      var imageId = innerSource.substring(21);
+      img.src = "https://thecatapi.com/api/images/get" + imageId + "&type=gif";
+    }
+  }
+  XHR.open("GET", "https://thecatapi.com/api/images/get?type=gif&format=xml");
+  XHR.send();
+});
