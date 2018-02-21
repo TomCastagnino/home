96f53354ab7648140c5566bda722731563700670
TomCastagnino <tcastagnino@gmail.com>
2018-02-21 21:23:01 +0000

    remuevo 'navbar fixed-top' para evitar problemas en mobile

--- public/js/cats.js
+++ public/js/cats.js
@@ -9,9 +9,13 @@ button.addEventListener("click", function() {
       var source = xml.getElementsByTagName("source_url");
       var  innerSource = source[0]["innerHTML"];
       var imageId = innerSource.substring(21);
-      img.src = "https://thecatapi.com/api/images/get" + imageId + "&type=gif&size=small";
+      img.src = "https://thecatapi.com/api/images/get" + imageId + "&size=small";
+      console.log("https://thecatapi.com/api/images/get"+imageId);
     }
-  }
+  };
   XHR.open("GET", "https://thecatapi.com/api/images/get?type=gif&format=xml");
   XHR.send();
 });
+
+
+//https://thecatapi.com/docs.html
